d1c8de5722cd0c9769c6768c1876122359b6ee64
Seraphania <strings333@hotmail.com>
2025-06-10 10:36:44 +0800

    chore: rebase main

---
+++ src/carpark.py
@@ -0,0 +1,10 @@
+class Carpark:
+    pass
+    # Attributes:
+    # location
+    # total_bays
+    # cars
+
+    # Methods:
+    # add_car -> update display
+    # remove_car -> update display
---
+++ tests/test_sensor.py
@@ -0,0 +1,9 @@
+import unittest
+from sensor import Sensor
+
+class testSensor(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
