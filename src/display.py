eb0560b2a5ac60140cce218fe5d693f5c11015b3
“Seraphania” <strings333@hotmail.com>
2025-06-14 19:19:47 +0800

    feat: add attributes and __str__ method to display class

--- src/display.py
+++ src/display.py
@@ -3,16 +3,18 @@ import carpark
 class Display:
     def __init__(self,
                  display_id: str,
-                 data: dict):
+                 message: str = "",
+                 is_on: bool = False):
         self.display_id = display_id
-        self.data = {"Location": '', # TODO: carpark.location implement this
-                     "Bays": "",
-                     "Message": '',
-                     "Temperature": '',
-                     }
+        self.message = message
+        self.is_on = is_on
+
+    def __str__(self) -> str:
+        return f"Display {self.display_id}: {self.message}"
+    
 
     def update_data(self):
-        pass # update infomration in the data dictionary # TODO: decide if this should be elsewhere
+        pass # update information in the data dictionary # TODO: decide if this should be elsewhere
 
     def display_data(self):
         pass # Print the display data in a nice way
