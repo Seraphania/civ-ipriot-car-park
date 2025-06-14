b90026333ead644705f902b2647ec5d6bc37f8fe
“Seraphania” <strings333@hotmail.com>
2025-06-14 18:47:17 +0800

    Style: Fix typos and formatting

--- src/carpark.py
+++ src/carpark.py
@@ -1,7 +1,8 @@
+# carpark.py
 class Carpark:
     def __init__(self,
                  location: str,
-                 capacity: int):
+                 capacity: int = 42):
 
         self.location = location
         self.capacity = capacity
@@ -9,8 +10,7 @@ class Carpark:
         self._displays = ''
 
     def __str__(self):
-        carpark_info = f"{self.location} carpark has a capacity of {self.capacity} bays."
-        return carpark_info
+        return f"{self.location} carpark has a capacity of {self.capacity} bays."
 
     def add_car(self):
         pass # Add a car to the carpark
@@ -18,5 +18,5 @@ class Carpark:
     def remove_car(self):
         pass # Remove a car from the carpark
     
-    def update_staus(self):
+    def update_status(self):
         pass # Update display information including available bays
---
+++ test/test_display.py
@@ -0,0 +1,9 @@
+import unittest
+from display import Display
+
+class testDisplay(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
