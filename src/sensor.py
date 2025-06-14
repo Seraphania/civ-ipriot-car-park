345819dc4b9fae38dd998e722c27eca817ba51a4
“Seraphania” <strings333@hotmail.com>
2025-06-14 19:41:30 +0800

    feat: Add constructors and attributes to sensor classes

--- src/sensor.py
+++ src/sensor.py
@@ -1,15 +1,26 @@
 class Sensor:
     def __init__(self,
                  sensor_id: str,
-                 active: bool):
+                 is_active: bool,
+                 carpark: str):
         self.sensor_id = sensor_id
-        self.active = True
+        self.is_active = is_active
+        self.carpark = carpark
 
-    def car_entry(self):
-        pass # update carpark when entry sensor is triggered
-
-    def car_exit(self):
-        pass # update carpark when exit sensor is triggered
+    def __str__(self):
+        return f"Sensor ID: {self.sensor_id}, Status: {self.is_active}"
     
     def update_staus(self):
         pass # Update display information
+
+class EntrySensor(Sensor):
+    ...
+    
+    def car_entry(self):
+        pass # update carpark when entry sensor is triggered
+
+class ExitSensor(Sensor):
+    ...
+
+    def car_exit(self):
+        pass # update carpark when exit sensor is triggered
