d258362b2a72d63a5b9530a836e8ad57aa1a22ac
“Seraphania” <strings333@hotmail.com>
2025-06-15 13:58:35 +0800

    feat: implement carpark class methods

--- src/display.py
+++ src/display.py
@@ -1,4 +1,5 @@
-import carpark
+from time import sleep
+from sys import stdout
 
 class Display:
     def __init__(self,
@@ -12,8 +13,10 @@ class Display:
     def __str__(self) -> str:
         return f"Display {self.display_id}: {self.message}"
 
-    def update(self):
-        pass # update information in the data dictionary
-
-    def display_data(self):
-        pass # Print the display data in a nice way
+    def update(self, data: dict, delay: float = 0.1, width: int = 40):
+        for key, value in data.items():
+            line = f"{' ' * width}{key}: {value}{' ' * width}"
+            for i in range(len(line) - width + 1):
+                stdout.write(f"|\r|{line[i:i+width]}")
+                stdout.flush()
+                sleep(delay)
