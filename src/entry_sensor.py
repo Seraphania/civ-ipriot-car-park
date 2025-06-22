6689322ff70865df5a5759e5a82ed819a8c32464
“Seraphania” <strings333@hotmail.com>
2025-06-22 17:27:48 +0800

    docs: Documentation added to all classes

--- src/entry_sensor.py
+++ src/entry_sensor.py
@@ -3,6 +3,9 @@ from sensor import Sensor
 class EntrySensor(Sensor):
 
     def update_car_park(self, plate, temperature):
+        """
+        Add car to list of carpark's plates, and provide an update on the current temperature
+        """
         self.carpark.temperature = temperature
         self.carpark.add_car(plate)
         
