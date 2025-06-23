bb31ada514392d9b1a815821379ecad8ab6f489b
“Seraphania” <strings333@hotmail.com>
2025-06-23 19:50:24 +0800

    feat: added carpark logging functionality and updated tests

--- src/carpark.py
+++ src/carpark.py
@@ -1,5 +1,6 @@
 from display import Display
 from datetime import datetime
+from pathlib import Path
 
 class Carpark:
     displays: list[Display]
@@ -7,12 +8,16 @@ class Carpark:
                  location: str,
                  capacity: int,
                  _plates = None,
-                 _displays = None):
+                 _displays = None,
+                 log_file=Path("log/log.txt")):
 
         self.location = location
         self.capacity = capacity
+        self.log_file = log_file
         self.plates = _plates or []
         self.displays = _displays or []
+        self.log_file.parent.mkdir(parents=True, exist_ok=True)
+        self.log_file.touch(exist_ok=True)
 
         self.message = f"Welcome to {self.location} carpark"
         self.temperature = ""
@@ -27,7 +32,13 @@ class Carpark:
         raw_time = datetime.now()
         time = raw_time.strftime("%I:%M %p")
         return time
-
+    
+    @property
+    def date(self) -> str:
+        raw_time = datetime.now()
+        date = f"{raw_time.day}/{raw_time.month}/{raw_time.year}"
+        return date
+    
     def __str__(self):
         return f"{self.location} carpark has a capacity of {self.capacity} bays."
     
@@ -38,7 +49,11 @@ class Carpark:
         if not isinstance(display, Display):
             raise TypeError("Component is not a display")
         self.displays.append(display)
-        
+
+    def _log_car(self, plate:str, entry=True):
+        with self.log_file.open(mode='a', encoding="utf8") as file:
+            file.write(f"Car {plate} {'entered' if entry else 'exited'} at {self.time} {self.date}\n")
+
     def add_car(self, plate: str):
         """
         Add plates to the list of plates in the carpark and trigger an update to displays
@@ -46,6 +61,7 @@ class Carpark:
         if plate in self.plates:
             raise ValueError("Vehicle is already in the carpark")
         self.plates.append(plate)
+        self._log_car(plate)
         self.update_displays()
 
     def remove_car(self, plate: str):
@@ -55,6 +71,7 @@ class Carpark:
         if plate not in self.plates:
             raise ValueError("This vehicle has not been registered in the carpark")
         self.plates.remove(plate)
+        self._log_car(plate, entry=False)
         self.update_displays()
 
     def update_displays(self, scroll_print=True):
@@ -69,3 +86,4 @@ class Carpark:
             "Message": self.message,
             }
             display.print_to_display(scroll=scroll_print)
+
